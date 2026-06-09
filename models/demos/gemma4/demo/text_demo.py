87fe2c3cc9108e12527ca67361130865908a4588
fbuticTT <ctr-fbutic@ext.tenstorrent.com>
2026-06-09 17:08:07 +0200

    refactoring

--- models/demos/gemma4/demo/text_demo.py
+++ models/demos/gemma4/demo/text_demo.py
@@ -20,7 +20,6 @@ import time
 import pytest
 import torch
 from loguru import logger
-import json
 
 import ttnn
 from models.demos.gemma4.tests.test_factory import PREFILL_BUCKETS, parametrize_mesh_with_fabric
